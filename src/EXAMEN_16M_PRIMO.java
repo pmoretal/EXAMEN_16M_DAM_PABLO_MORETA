256273636f41126bfb4d00ed3c9fb6537a5c4927
pmoretal <pmoretal01@ceinmark.net>
2022-05-16 09:12:00 +0200

    Primer commit Pablo

---
+++ src/EXAMEN_16M_PRIMO.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EXAMEN_16M_PRIMO {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner (System.in);
+		int num1=0;
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+		num1=ent.nextInt();
+	}
+
+}
